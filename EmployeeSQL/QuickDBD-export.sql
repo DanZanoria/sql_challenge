59d309fe193e308d4c14999c03a9b6d0912f66e8
DanZanoria <71779837+DanZanoria@users.noreply.github.com>
2021-01-16 17:50:30 -0500

    more images
    imported postGress

---
+++ EmployeeSQL/QuickDBD-export.sql
@@ -0,0 +1,45 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" int   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" int   NOT NULL,
+    "title" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" money   NOT NULL
+);
+
+CREATE TABLE "departments" (
+    "dept_no" int   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "dept_empl" (
+    "emp_no" int   NOT NULL,
+    "dept_no" int   NOT NULL
+);
+
+CREATE TABLE "dept_man" (
+    "dept_no" int   NOT NULL,
+    "emp_no" int   NOT NULL
+);
+
